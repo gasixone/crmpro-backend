0e78577b683f8e1b560c66869b63466cde8ba44f
gasixone <gasixone@gmail.com>
2026-02-11 18:48:31 +0300

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+# 1. Klasöre git
+cd /mnt/okcomputer/output/crmpro-backend
 
+# 2. Git repo'su başlat
+git init
+
+# 3. GitHub repo'suna bağlan
+git remote add origin https://github.com/gasixone/crmpro-backend.git
+
+# 4. Tüm dosyaları ekle
+git add .
+
+# 5. Commit yap
+git commit -m "Initial commit - CRMPro Backend API"
+
+# 6. GitHub'a gönder
+git push -u origin main
